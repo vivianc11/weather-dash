df2c5f6a9e106c297fc6bc51203f6c0f122ef8f4
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-13 16:06:02 -0700

    updated README with info about the project and a link to the deployed app. Added comments to HTML and JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,9 @@
-
+// Targeting classes set in HTML
 var searchBtn = document.querySelector('.search-btn');
 var searchHistory = document.querySelector('.search-history');
 var currentDate = document.querySelector('.current-date');
 var currentWeather = document.querySelector('.current-weather');
-var futureWeather = document.querySelector('.future-weather.row');
+var futureWeather = document.querySelector('.future-weather');
 var locationDate = document.querySelector('.location-date');
 var currentTemp = document.querySelector('.current-temp');
 var currentWind = document.querySelector('.current-wind');
@@ -11,6 +11,8 @@ var currentHumidity = document.querySelector('.current-humidity');
 var currentUV = document.querySelector('.current-UV');
 var weatherIcon = document.querySelector('.weather-icon');
 var myKey = config.MY_KEY;
+
+// Creating an element and setting attribute
 var historyBtn = document.createElement('button');
     historyBtn.setAttribute('class', 'btn btn-secondary');
 
@@ -26,43 +28,55 @@ var coord = [];
 var timeStamp;
 var forcastDate;
 
-
+// Displaying the current date
 currentDate.textContent = moment().format('dddd, MM/DD/YY');
 
+// Function to get the latitude and longitude from an entered city
 function getCoord() {
     var requestLocation = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${myKey}`
 
     fetch(requestLocation)
     .then(function (response) {
-        return response.json();
+        if (response){
+            return response.json();
+        } else {
+            console.log("err")
+        }   
     })
     .then(function (data) {
-        console.log(data);
         latitude = data[0].lat;
         longitude = data[0].lon;
         place = data[0].name;
+        // Executing getWeather function
         getWeather(latitude, longitude, place);
     })  
 }
 
+// Function to use values from getCoord function to get weather information
 function getWeather(latitude, longitude, place) {
     
     var requestUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${myKey}`;
 
     fetch(requestUrl)
         .then(function (response) {
-            return response.json();
+            if (response) {
+                return response.json();
+            } else {
+                console.log(err);
+            }
         })
         .then(function (data) {
-            console.log(data);
-
+            // Getting the weather icon value and placing it into the URL to get the associated icon
             iconVal = data.current.weather[0].icon;
-            locationDate.textContent = place;
                 weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${iconVal}@4x.png`);
+            
+            // Displaying weather info
+            locationDate.textContent = place;
             currentTemp.textContent = `Temp: ${data.current.temp}`
             currentWind.textContent = `Wind: ${data.current.wind_speed} mph`;
             currentHumidity.textContent = `Humidity: ${data.current.humidity}`;
             currentUV.textContent = data.current.uvi;
+            // Background colors associted with the UV index
             if (data.current.uvi > 10) {
                 currentUV.style.backgroundColor = "pink";
                 currentUV.style.color = "white";
@@ -79,28 +93,29 @@ function getWeather(latitude, longitude, place) {
                 currentUV.style.backgroundColor = "green";
                 currentUV.style.color = "white";
             }
-
+            // Displaying 5 cards to display future weather info
+            // Using .map to loop through the object
             futureWeather.innerHTML = data.daily.map((day, index) => {
                 if (index < 5){
-                    timeStamp = day.dt * 1000;
-                    forcastDate = new Date(timeStamp).toString();
-                    forcastDate = forcastDate.slice(4, 15);
+                    timeStamp = day.dt * 1000; // Multiplying the timestamp by 1000 converts it so JS can read it appropriately
+                    forcastDate = new Date(timeStamp).toString(); // Converting to string type so then it can be sliced
+                    forcastDate = forcastDate.slice(4, 15); // Slicing because only part of the date is needed
                     
                     return `<div class="weather-card card border-dark m-1 col-md-2 col-sm-12" style="max-width: 30rem;">
                     <div class="card-header">${forcastDate}</div>
                     <div class="card-body text-dark">
-                      <img class="img-fluid" src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon">
+                      <img class="img-fluid rounded" src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon">
                       <p class="card-text">Temp: ${day.temp.day}</p>
                       <p class="card-text">Wind: ${day.wind_speed} mph</p>
                       <p class="card-text">Humidity: ${day.humidity}</p>
                     </div>
                     </div>`;
                 }
-            }).join(' ')
+            }).join(' ') // Joining the cards together with a space in between
         })  
 }
 
-
+// Function to update the search history
 function updateHistory () {
    var fromStorage = JSON.parse(localStorage.getItem('location'));
    var index = fromStorage.length - 1
@@ -113,6 +128,7 @@ function updateHistory () {
    searchHistory.appendChild(historyItems);
 }
 
+// Function to load the search history if there is already locations saved
 function loadHistory () {
     if (localStorage !== null){
         var fromStorage = JSON.parse(localStorage.getItem('location'));
@@ -120,7 +136,7 @@ function loadHistory () {
             var historyItems = document.createElement('li');
             historyItems.setAttribute('class', 'history-items d-grid gap-2');
             historyBtn = document.createElement('button');
-            historyBtn.setAttribute('class', 'btn btn-secondary');
+            historyBtn.setAttribute('class', 'btn btn-secondary mb-1');
             historyBtn.textContent = `${fromStorage[i]}`;
             historyItems.appendChild(historyBtn);
             searchHistory.appendChild(historyItems);
@@ -128,7 +144,7 @@ function loadHistory () {
     }
 }
 
-
+// What happens when the search button is clicked
 searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
     cityInput = document.getElementById('city-input').value.trim();
@@ -140,10 +156,11 @@ searchBtn.addEventListener('click', (event) => {
     document.getElementById('city-input').value = '';
 })
 
-
+// What happens when a city under the search history is clicked
 searchHistory.addEventListener('click', (event) => {
     city = event.target.textContent;
     getCoord();
 })    
 
+// If there is a location saved in the localStorage, it will be displayed when the page loads/refreshes
 loadHistory();
--- README.md
+++ README.md
@@ -1,5 +1,53 @@
 # weather-dash
 
+## About the Project
+This weather dashboard (styled by Bootstrap CSS Framework) allows users to input a city into the search field and will present current and future weather conditions for that city on the webpage. That city is then displayed on the search history, which can be clicked on to get weather information on that city once more. 
+Since the weather API only takes in latitude and longitude for their parameters, the coordinates for the city is retrieved from a Geocoding API provided by OpenWeather. The weather information is then recieved from OneCall API, also from OpenWeather.
+
+## Building the Weather Dashboard
+The entirety of this project was created from scratch to meet the following requirements:
+
+- When the user searches a for a city, the page presents with current and future conditions for that city and that city is added to the search history
+
+- When viewing the current weather contidtions for that city, the user is presented with 
+    - city name
+    - the date
+    - an icon representation of weather condition
+    - the temperature, the humidity
+    - the wind speed
+    - the UV index (has a color that indicates whether the conditions are favorable, moderate, or severe)
+
+- When viewing the future weather conditions for that city, the user is presented with a 5-day forecast that displays 
+    - the date
+    - an icon representation of weather conditions
+    - the temperature
+    - the wind speed
+    - the humidity
+
+- When a city in the search history is clicked, the user is again presented with current and future conditions for that city
+
+## Applied Skills
+- Using Bootstrap CSS Framework for the majority of the styling
+
+- DOM manipulation to target specific classes, creating elements, setting their attributes, appending the new elements on the page, and modifying the text content.
+
+- Practice using moment.js to display the current date
+
+- Using fetch to retrieve information from an API and then handling the reponse and data
+
+- Getting data from one API to use in a separate API
+
+- Using .map() to loop through an object and returning a card that has specific information pulled from the object and then .join() the cards together to be displayed on the page
+
+- Storing the searched cities into the user's local storage and then getting that value from the local storage to be displayed in the search history container. Used JSON.stringify and JSON.parse to put items and pull items out of the local storage respectively.
+
+- Using event listeners on the search button and also on cities displayed in the search history so that when the user clicks on those buttons, the correct weather info gets populated.
+
+## Deployed Project
+https://vivianc11.github.io/weather-dash/
+
+![image of weather dashboard](./Assets/Screen%20Shot%202022-09-13%20at%203.32.06%20PM.png)
+
 ### Resources
 https://momentjs.com/
 https://www.youtube.com/watch?v=nGVoHEZojiQ&ab_channel=SteveGriffith-Prof3ssorSt3v3
